8988c6ca4c599ed7b1ecd36ab08371d8fd543238
Toni, Uusimäki <aa4350@student.jamk.fi>
2021-04-08 09:36:18 +0300

    organized things

---
+++ src/Button.svelte
@@ -0,0 +1,22 @@
+<button on:click><slot>Tyhjennä</slot></button>
+
+<style>
+  button {
+    display: inline-block;
+    padding: 0.35em 1.2em;
+    border: 0.1em solid #ffffff;
+    margin: 0 0.3em 0.3em 0;
+    border-radius: 0.12em;
+    box-sizing: border-box;
+    text-decoration: none;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
+    color: #ff3e00;
+    text-align: center;
+    transition: all 0.2s;
+  }
+  button:hover {
+    color: #000000;
+    background-color: #bbbbbb;
+  }
+</style>
